1cfb67c4208be1824b176b39127ca804eb19952e
thelouisgram <hadesanoye01@gmail.com>
2023-04-09 23:04:14 +0100

    adjusted cart

--- e-commerce-product-page/src/components/Cart.jsx
+++ e-commerce-product-page/src/components/Cart.jsx
@@ -4,7 +4,7 @@ const Cart = ({ activeCart, setActiveCart }) => {
 	return (
 		<section className="top-[80px] px-6 md:p-0 w-full absolute flex  md:justify-end font-Kumbh md:right-[100px] md:top-[90px]">
 			<div className="bg-white rounded-[10px] md:w-[300px] box  w-full ">
-				<div className="border-b-[1px] p-4 border-lightGrayishBlue">
+                <div className="border-b-[1px] p-4 border-veryDarkBlue">
 					<h4 className="font-[700] text-veryDarkBlue">Cart</h4>
 				</div>
 				<div className="min-h-[175px] p-4 flex justify-center items-center">
